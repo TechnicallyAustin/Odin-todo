632e28dc9ab64831579dc386a2525bc9029b2dfa
tehcnicallyAustin <Technicallyaustin@gmail.com>
2023-03-15 23:43:28 -0700

    Created page objects

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -2,8 +2,6 @@ import "bootstrap";
 import "jquery"
 import 'popper.js'
 import "../css/styles.css";
-import { test } from "./page.js";
+import { Page } from "./page.js";
 
-console.log("Welcome");
 
-test();
--- src/javascript/page.js
+++ src/javascript/page.js
@@ -1,8 +1,26 @@
+// creates the Page and calls abstracted functions to create section info
 export const Page = {
+    page: document.querySelector(".page-content"),
     content: null,
-    header: function () {},
-    main: function () {},
-    footer: function () {}
+    header: function () {
+        const header = this.page.appendChild(document.createElement("header"));
+        header.setAttribute("class", "")
+        navbar.logo()
+        navbar.links()
+    },
+    main: function () {
+        const main = this.page.appendChild(document.createElement("main"));
+        main.setAttribute("class", "")
+        mainElements.newSection()
+        mainElements.article()
+        },
+    footer: function () {
+        const footer = this.page.appendChild(document.createElement("footer"));
+        footer.setAttribute("class", "")
+        footerElements.footer()
+        footerElements.info()
+        footerElements.text()
+    }
 };
 
 // creates a navBar and appends it to the header
@@ -18,5 +36,13 @@ const mainElements = {
     content: null,
     newSection: function(){}, // new section is set to Content -- Task Folder
     article: function(){} // appends an article to content. -- Tasks
+}
+
+// creates the footer elements 
+const footerElements = {
+    content: null,
+    footer: function(){},
+    info: function(){},
+    text: function(){}
 }
 
