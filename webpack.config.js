e89a36bf367f64ba1b3f5c77e3c3d23f404f0d7e
tehcnicallyAustin <Technicallyaustin@gmail.com>
2023-03-15 21:08:22 -0700

    webpack setup completed

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,45 @@
 const path = require('path')
 
 module.exports = {
-    entry: './src/javascript/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+  entry: "./src/javascript/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      // Add individual asset loaders here
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
     },
-    module: {
-        rules: [
-            // Add individual asset loaders here
-        ]
+    {
+      test: /\.(scss)$/,
+      use: [
+        {
+          loader: "style-loader", // inject CSS to page
+        },
+        {
+          loader: "css-loader", // translates CSS into CommonJS modules
+        },
+        {
+          loader: "postcss-loader", // Run post css actions
+          options: {
+            plugins: function () {
+              // post css plugins, can be exported to postcss.config.js
+              return [require("precss"), require("autoprefixer")];
+            },
+          },
+        },
+        {
+          loader: "sass-loader", // compiles Sass to CSS
+        },
+      ],
+    },
+    {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource' // loads images
     }
-    
+    ],
+  },
 };
