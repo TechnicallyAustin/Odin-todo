90c241c8bfe2b8718f133b69338528e086d9fe25
tehcnicallyAustin <Technicallyaustin@gmail.com>
2023-03-15 22:34:55 -0700

    creating a basic page JS/ HTML page set up

--- src/javascript/page.js
+++ src/javascript/page.js
@@ -1,6 +1,22 @@
-export function test(){
-    const test = document.body.appendChild(document.createElement("h1"));
-    test.textContent = "Welcome to Todo";
-    test.setAttribute("class", "bg-primary");
-    console.log(test);
+export const Page = {
+    content: null,
+    header: function () {},
+    main: function () {},
+    footer: function () {}
+};
+
+// creates a navBar and appends it to the header
+const navbar = {
+    content: null,
+    logo: function(){},
+    links: function(){}
 }
+
+// creates a section and article within the Main section
+const mainElements = {
+    main: document.querySelector("main"),
+    content: null,
+    newSection: function(){}, // new section is set to Content -- Task Folder
+    article: function(){} // appends an article to content. -- Tasks
+}
+
