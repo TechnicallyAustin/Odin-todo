88e29c54e288f685c7980412b8453cd5d8a0a350
tehcnicallyAustin <Technicallyaustin@gmail.com>
2023-03-23 16:56:06 -0700

    created psuedo code for new task module

--- src/javascript/tasks/newTask.js
+++ src/javascript/tasks/newTask.js
@@ -1,3 +1,29 @@
 function newTask(){
-    
+    // creates a modal containing a form
+
+    // Modal
+        // div class modal
+            // div class modal dialouge
+                // div class modal content
+                    // div class modal header
+                        // div class modal title
+                            // button class btn close
+        
+        // div class modal body
+            // form
+                // inputs
+                    // title
+                    // info
+                    // tags
+                    // date
+                    // completed
+                    // color
+                // submit
+
+        //div class modal footer
+            // button class btn close
+            // button class btn save
+                // submits form and calls a function to create Tasks that also creates cards.
+
+    // on submit create a task with the inputs of the modal
 }
