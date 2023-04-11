a8bdf06cb999361e69fc25eda74ca567425f19cd
tehcnicallyAustin <Technicallyaustin@gmail.com>
2023-04-11 14:27:34 -0700

    removed form code from modal

--- src/javascript/tasks/form.js
+++ src/javascript/tasks/form.js
@@ -5,7 +5,7 @@ export function newTaskForm() {
     form: null,
     createForm: function () {
       const form = body.appendChild(document.createElement("form"));
-      form.setAttribute("class", "task-form h-100 w-100");
+      form.setAttribute("class", " d-flex flex-column justify-content-around align-items-center task-form h-100 w-100");
       this.form = form;
     },
     legend: function () {
@@ -19,7 +19,7 @@ export function newTaskForm() {
             let value = title.toLowerCase()
 
             let fieldset = this.form.appendChild(document.createElement("fieldset"))
-            fieldset.setAttribute("class", "")
+            fieldset.setAttribute("class", " w-75 d-flex justify-content-between align-items-center")
             fieldset.setAttribute("id", "")
 
             let label = fieldset.appendChild(document.createElement("label"))
@@ -27,40 +27,47 @@ export function newTaskForm() {
             label.setAttribute("id", "")
             label.textContent = value
 
-            let input = fieldset.appendChild(document.createElement("input"))
-            input.setAttribute("class", "")
+            // let input = fieldset.appendChild(document.createElement("input"))
+            //input.setAttribute("class", "")
 
             switch (value){
                 case "title":
-
+                    let title = fieldset.appendChild(document.createElement("input"))
                     break;
                 
                 case "info":
-
+                    let info = fieldset.appendChild(document.createElement("textarea"))
+                    info.setAttribute("class", "w-75 d-flex justify-content-around align-items-center")
+                    info.setAttribute("type", "textarea")
+                    info.setAttribute("rows", "5")
+                    info.setAttribute("cols", "30")
                     break;
                 
                 case "tags":
-
+                      let tags = fieldset.appendChild(document.createElement("input"))
                     break;
 
                 case "date":
-                    input.setAttribute("class", "");
-                    input.setAttribute("type", "date")
+                    let date = fieldset.appendChild(document.createElement("input"))
+                    date.setAttribute("class", "");
+                    date.setAttribute("type", "date")
 
                 break;
 
                 case "complete":
-                    fieldset.setAttribute("class", "form-check form-switch")
-                    input.setAttribute("class", "form-check-input")
-                    input.setAttribute("type", "checkbox")
-                    input.setAttribute("value", "")
-                    input.setAttribute("unchecked","")
+                    let complete = fieldset.appendChild(document.createElement("input"))
+                    fieldset.setAttribute("class", "p-0 w-75 d-flex justify-content-between align-items-center form-check form-switch")
+                    complete.setAttribute("class", "form-check-input")
+                    complete.setAttribute("type", "checkbox")
+                    complete.setAttribute("value", "")
+                    complete.setAttribute("unchecked","")
                 break;
 
                 case "color":
-                    input.setAttribute("class", "form-control form-control-color");
-                    input.setAttribute("type", "color")
-                    input.setAttribute("value", "#FFFFFF")
+                    let color = fieldset.appendChild(document.createElement("input"))
+                    color.setAttribute("class", "form-control form-control-color");
+                    color.setAttribute("type", "color")
+                    color.setAttribute("value", "#FFFFFF")
                 break;
 
                 default:
--- src/javascript/tasks/newTask.js
+++ src/javascript/tasks/newTask.js
@@ -5,7 +5,7 @@ const modal = {
   modal: null,
   createModal: function () {
     const modal = document.createElement("div");
-    modal.setAttribute("class", "modal w-50 h-50 bg-dark p-2");
+    modal.setAttribute("class", "modal w-25 h-50 bg-dark p-2");
     this.page.appendChild(modal);
     this.modal = modal;
   },
@@ -34,57 +34,10 @@ const modal = {
       "modal-body h-100 d-flex flex-column justify-content-around align-items-start"
     );
     newTaskForm()
-
-    //const form = body.appendChild(document.createElement("form"));
-    //form.setAttribute("class", "task-form h-100 w-100")
-//
-    //const legend = form.appendChild(document.createElement("p"));
-    //legend.setAttribute("class", "legend text-white mb-0 fs-3");
-    //legend.textContent = "Add a New Task";
-//
-    //const inputs = ["Title", "Info", "Tags", "Date", "Complete", "Color"];
-    //for (let input of inputs) {
-    //  let value = input.toLowerCase();
-//
-    //  let fieldset = document.createElement("fieldset");
-    //  fieldset.setAttribute(
-    //    "class",
-    //    "d-flex justify-content-around align-items-center w-100"
-    //  );
-    //  fieldset.setAttribute("class", `${value}-field d-flex justify-content-around align-items-center`);
-//
-    //  let label = fieldset.appendChild(document.createElement("label"));
-    //  label.setAttribute("class", "text-white w-25");
-    //  label.setAttribute("for", `${value}`);
-    //  label.textContent = value;
-//
-    //  let formInput = fieldset.appendChild(document.createElement("input"));
-    //  formInput.setAttribute("class", " h-75 w-50")
-    //  formInput.setAttribute("type", "text");
-    //  formInput.setAttribute("id", "");
-//
-    //  form.appendChild(fieldset);
-    //}
-    //const submit = form.appendChild(document.createElement("button"))
-    //submit.setAttribute("value", "submit")
-    //submit.textContent = "Add Task"
   },
   footer: function () {
-    //const form = document.querySelector("form");
     const footer = document.createElement("footer");
     footer.setAttribute("class", "modal-footer  h-50 w-100");
-    // end form here
-
-    //const submit = footer.appendChild(document.createElement("button"));
-    //submit.setAttribute("value", "submit");
-    //submit.setAttribute("class", "modal-btn btn h-100 w-100 btn-success")
-    //submit.textContent = "Add Task";
-//
-    //const cancel = footer.appendChild(document.createElement("button"));
-    //cancel.setAttribute("value", "Cancel")
-    //cancel.setAttribute("class", "modal-btn btn btn-danger h-100 w-100")
-    //cancel.textContent = "Cancel"
-
     this.modal.appendChild(footer);
   },
 };
