0709683f7bb56e6c42eebc85fdc6061e1ce9e944
tehcnicallyAustin <Technicallyaustin@gmail.com>
2023-04-13 16:42:04 -0700

    staging main content changes

--- src/javascript/content/content.js
+++ src/javascript/content/content.js
@@ -1,7 +1,17 @@
 export function content(){
 
     const page = {
-        
+        mainHeader: function(){
+            const elements = {
+                optionBar: function(){},
+                viewIcon: function(){}
+            }
+            elements.optionBar()
+            elements.viewIcon()
+        },
+        todo: function(){},
+        doing: function(){},
+        done: function(){}
     }
 
 
